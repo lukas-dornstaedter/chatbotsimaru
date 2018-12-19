487d8dce52dd5e7f76e3e31717c353be5081b6b8
Lukas Dornstaedter <lukas.dornstaedter@gmail.com>
2018-12-19 22:10:29 +0100

    newOrder-set-update

--- app.js
+++ app.js
@@ -232,7 +232,7 @@ function handleDialogFlowAction(
 
         if (isDefined(contexts[0].parameters.fields["orderNumber"])) {
           let orderNumber = jouput.fields["orderNumber"].stringValue;
-          zipCode = jouput.fields[zipCode].stringValue;
+          zipCode = jouput.fields[zipCode].numberValue;
 
           var request = require("request"),
             username = config.BILLBEE_USERNAME,
