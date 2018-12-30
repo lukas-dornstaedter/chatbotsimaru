bdaffb3bc319169b2ae3ac950ac9319ce236da24
Lukas Dornstaedter <lukas.dornstaedter@gmail.com>
2018-12-30 13:57:15 +0100

    open webvie

---
+++ routes/webviews.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const config = require("../config");
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/webview", function(req, res) {
+  res.render("retour-settings");
+});
+
+module.exports = router;
