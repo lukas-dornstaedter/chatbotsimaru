e78b39956e61ac9c1195a4fd06de11d05f6c2d45
Lukas Dornstaedter <lukas.dornstaedter@gmail.com>
2018-12-30 17:43:14 +0100

    webview load data

--- routes/webviews.js
+++ routes/webviews.js
@@ -9,4 +9,30 @@ router.get("/webview", function(req, res) {
   res.render("retour-settings");
 });
 
+router.get("/settings", function(req, res) {
+  var request = require("request"),
+    username = config.BILLBEE_USERNAME,
+    password = config.BILLBEE_PASS,
+    url = "https://app.billbee.io/api/v1/orders/findbyextref/7191",
+    auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+  request(
+    {
+      url: url,
+      headers: {
+        Authorization: auth,
+        "X-Billbee-Api-Key": config.BILLBEE_API_KEY,
+        Accept: "application/json"
+      }
+    },
+    function(error, response, body) {
+      // Do more stuff with 'body' here
+      //console.log(body.Data);
+      let data = JSON.parse(body);
+
+      res.json(data.Data.OrderItems);
+    }
+  );
+});
+
 module.exports = router;
