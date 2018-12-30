240a145c36dbe473390fd0dc2beed4549b373718
Lukas Dornstaedter <lukas.dornstaedter@gmail.com>
2018-12-30 18:08:43 +0100

    webview load data

--- routes/webviews.js
+++ routes/webviews.js
@@ -2,6 +2,7 @@
 
 const config = require("../config");
 const express = require("express");
+const fbservice = require("../services/fb-service");
 
 const router = express.Router();
 
@@ -10,30 +11,8 @@ router.get("/webview", function(req, res) {
 });
 
 router.get("/settings", function(req, res) {
-  console.log("hallo");
-  var request = require("request"),
-    username = config.BILLBEE_USERNAME,
-    password = config.BILLBEE_PASS,
-    url = "https://app.billbee.io/api/v1/orders/findbyextref/7191",
-    auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
-
-  request(
-    {
-      url: url,
-      headers: {
-        Authorization: auth,
-        "X-Billbee-Api-Key": config.BILLBEE_API_KEY,
-        Accept: "application/json"
-      }
-    },
-    function(error, response, body) {
-      // Do more stuff with 'body' here
-      //console.log(body.Data);
-      let data = JSON.parse(body);
-      console.log(data.Data.OrderItems);
-      res.json(data.Data.OrderItems);
-    }
-  );
+  let response = `Newsletter wie geht es dir`;
+  res.send(response);
 });
 
 module.exports = router;
