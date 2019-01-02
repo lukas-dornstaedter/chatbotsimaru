a40b5d745a0e82e1006aaee790398dc2fea8a8fe
Lukas Dornstaedter <lukas.dornstaedter@gmail.com>
2019-01-02 20:05:59 +0100

    submit button

--- routes/webviews.js
+++ routes/webviews.js
@@ -38,4 +38,41 @@ router.get("/settings", function(req, res) {
   //res.json([]);
 });
 
+router.get("/announce-return", function(req, res) {
+  var request = require("request"),
+    username = config.BILLBEE_USERNAME,
+    password = config.BILLBEE_PASS,
+    url = "https://app.billbee.io/api/v1/orders/54765571/tags",
+    auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+  request({
+    url: url,
+    methode: "POST",
+    headers: {
+      Authorization: auth,
+      "X-Billbee-Api-Key": config.BILLBEE_API_KEY,
+      Accept: "application/json"
+    },
+    multipart: [
+      {
+        "content-type": "application/json",
+        body: JSON.stringify({
+          Tags: ["return-announced"]
+        })
+      }
+    ],
+
+    function(error, response, body) {
+      // Do more stuff with 'body' here
+      //console.log(body.Data);
+      if (response.statusCode == 201) {
+        console.log("ok");
+      } else {
+        console.log("error: " + response.statusCode);
+        console.log(body);
+      }
+    }
+  });
+});
+
 module.exports = router;
